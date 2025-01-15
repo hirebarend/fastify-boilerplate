66f6c0af7dd728d4fd0688c181a2b384af99f5bd
Barend Erasmus <hirebarend@gmail.com>
2025-01-15 14:28:48 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,28 @@ import * as os from 'os';
 import { startServer } from './server';
 import { job } from './job';
 
+const regExp: RegExp = new RegExp(/^\-\-([^=]+)=(.+)$/);
+
+for (const arg of process.argv.slice(2)) {
+  const regExpExecArray: RegExpExecArray | null = regExp.exec(arg);
+
+  if (!regExpExecArray) {
+    throw new Error(`unable to parse argument: ${arg}`);
+  }
+
+  process.env[regExpExecArray[1].toUpperCase()] = regExpExecArray[2];
+}
+
 if (process.env.JOB) {
-  job();
+  (async () => {
+    while (true) {
+      await job();
+
+      for (let i = 0; i < 5 * 60; i++) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+    }
+  })();
 } else {
   if (process.env.CLUSTER) {
     if (cluster.isPrimary) {
