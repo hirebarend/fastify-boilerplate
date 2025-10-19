84b6bd27a7ec0d3d7ce64c84831e07e28897fdf4
Barend Erasmus <hirebarend@gmail.com>
2025-10-19 18:14:33 +0200

    fixes

--- src/routes/sessions-id-query-post.ts
+++ src/routes/sessions-id-query-post.ts
@@ -3,7 +3,7 @@ import * as crypto from 'crypto';
 import duckdb, { DuckDBConnection } from '@duckdb/node-api';
 
 import type { FastifyReply, FastifyRequest, RouteOptions } from 'fastify';
-import { getContainer, Query } from '../core';
+import { getContainer, Query, type File } from '../core';
 
 function normalizeFilename(str: string): string {
   const strSplitted = str.split('.');
@@ -13,7 +13,7 @@ function normalizeFilename(str: string): string {
     .replace(/[\u0300-\u036f]/g, '')
     .toLowerCase()
     .trim()
-    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/[^a-z0-9]+/g, '_')
     .replace(/^-+|-+$/g, '');
 }
 
@@ -33,83 +33,83 @@ export const SESSIONS_ID_QUERY_POST: RouteOptions<any, any, any, any> = {
 
     const connection = await DuckDBConnection.create();
 
-    const files = [
-      {
-        name: 'weather.csv',
-        url: 'https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2024-financial-year-provisional/Download-data/annual-enterprise-survey-2024-financial-year-provisional.csv',
-      },
-    ];
+    try {
+      const files = await container.db
+        .collection<File>('files')
+        .find({
+          'session.id': request.params.id,
+        })
+        .toArray();
 
-    // const files = await container.db
-    //   .collection('files')
-    //   .find({
-    //     'session.id': request.params.id,
-    //   })
-    //   .toArray();
-
-    for (const file of files) {
-      await connection.run(`
+      for (const file of files) {
+        try {
+          await connection.run(`
         CREATE TEMP VIEW "${normalizeFilename(file.name)}" AS
         SELECT * FROM read_csv_auto('${file.url}', header=true)
       `);
+        } catch {}
+      }
+
+      const start = new Date().getTime();
+      const readerResult = await connection.runAndReadAll(request.body.query);
+      const end = new Date().getTime();
+      const elapsed = end - start;
+
+      const columns = readerResult.columnNames();
+
+      const rows = readerResult.getRowsJson();
+
+      const buffer = Buffer.from(
+        [columns.join(','), ...rows.map((row) => row.join(','))].join('\n'),
+      );
+
+      const s3Client = new S3Client({ region: process.env.AWS_REGION });
+
+      const hash: string = crypto
+        .createHash('md5')
+        .update(buffer)
+        .digest('hex');
+
+      await s3Client.send(
+        new PutObjectCommand({
+          ACL: 'public-read',
+          Body: buffer,
+          Bucket: process.env.AWS_S3_BUCKET,
+          ContentType: 'text/csv',
+          Key: hash,
+        }),
+      );
+
+      const query: Query = {
+        contentType: 'text/csv',
+        hash,
+        id: faker.string.alphanumeric({
+          casing: 'lower',
+          length: 8,
+        }),
+        metadata: {
+          columns,
+          count: rows.length,
+          elapsed,
+        },
+        name: '',
+        query: request.body.query,
+        session: {
+          id: request.params.id,
+        },
+        size: buffer.length,
+        url: `https://${process.env.AWS_S3_BUCKET}.s3.amazonaws.com/${hash}`,
+      };
+
+      await container.db.collection<Query>('queries').insertOne(query);
+
+      reply.status(200).send({
+        ...query,
+        rows,
+      });
+    } finally {
+      connection.closeSync();
     }
-
-    const start = new Date().getTime();
-    const readerResult = await connection.runAndReadAll(request.body.query);
-    const end = new Date().getTime();
-    const elapsed = end - start;
-
-    const columns = readerResult.columnNames();
-
-    const rows = readerResult.getRowsJson();
-
-    connection.closeSync();
-
-    const buffer = Buffer.from(
-      [columns.join(','), ...rows.map((row) => row.join(','))].join('\n'),
-    );
-
-    const s3Client = new S3Client({ region: process.env.AWS_REGION });
-
-    const hash: string = crypto.createHash('md5').update(buffer).digest('hex');
-
-    await s3Client.send(
-      new PutObjectCommand({
-        ACL: 'public-read',
-        Body: buffer,
-        Bucket: process.env.AWS_S3_BUCKET,
-        ContentType: 'text/csv',
-        Key: hash,
-      }),
-    );
-
-    const query: Query = {
-      contentType: 'text/csv',
-      hash,
-      id: faker.string.alphanumeric({
-        casing: 'lower',
-        length: 8,
-      }),
-      metadata: {
-        columns,
-        count: rows.length,
-        elapsed,
-      },
-      name: '',
-      query: request.body.query,
-      session: {
-        id: request.params.id,
-      },
-      size: buffer.length,
-      url: `https://${process.env.AWS_S3_BUCKET}.s3.amazonaws.com/${hash}`,
-    };
-
-    await container.db.collection<Query>('queries').insertOne(query);
-
-    reply.status(200).send({
-      ...query,
-      rows,
-    });
   },
   method: 'POST',
   url: '/api/v1/sessions/:id/query',
